192422a8d1ad2778e2565219029dbd09fef9d5b1
Jesrriel Moura <jesrrielriri@gmail.com>
2025-10-23 17:14:39 +0000

    casos de test

--- Triangulo.py
+++ Triangulo.py
@@ -1,33 +1,48 @@
 def ladosTriangulo(a, b, c):
+    """Determina o tipo de triângulo com lados a, b, c.
+
+    Aceita números (int/float) ou strings numéricas. Retorna uma string com a
+    classificação: 'Equilátero', 'Isóceles', 'Escaleno', 'Não é possível formar um triangulo'
+    ou 'Não é válido' quando os valores são inválidos.
+    """
+    # tentar converter entradas que são strings numéricas
+    try:
+        a_num = float(a)
+        b_num = float(b)
+        c_num = float(c)
+    except Exception:
+        return "Não é válido"
+
+    a = a_num
+    b = b_num
+    c = c_num
+
     if (a + b > c) and (a + c > b) and (b + c > a) and a > 0 and b > 0 and c > 0:
-        if a == b and b == c and c == b:
-            print("Equilatéro")
+        if a == b and b == c:
+            return "Equilátero"
         elif (a == b and b != c) or (a == c and a != b) or (c == b and a != b):
-            print("Isóceles")
+            return "Isóceles"
         elif a != b and b != c and a != c:
-            print("Escaleno")
+            return "Escaleno"
         else:
-            print("Não é válido")
+            return "Não é válido"
     else:
-        print("Não é possível formar um triangulo")
+        return "Não é possível formar um triangulo"
 
-a = input("Digite o primeiro valor: ")
-while True:
-    if (not a):
+
+def _main():
+    a = input("Digite o primeiro valor: ")
+    while not a:
         a = input("Digite o primeiro valor: ")
-    elif (a):
-        break
-b = input("Digite o segundo valor: ")
-while True:
-    if (not b):
+    b = input("Digite o segundo valor: ")
+    while not b:
         b = input("Digite o segundo valor: ")
-    elif (b):
-        break
-c = input("Digite o terceiro valor: ")
-while True:
-    if (not c):
+    c = input("Digite o terceiro valor: ")
+    while not c:
         c = input("Digite o terceiro valor: ")
-    elif (c):
-        break
 
-ladosTriangulo(a, b, c)
+    print(ladosTriangulo(a, b, c))
+
+
+if __name__ == "__main__":
+    _main()
---
+++ test_triangulo.py
@@ -0,0 +1,34 @@
+import unittest
+import unity
+from Triangulo import ladosTriangulo
+
+
+class TestTriangulo(unittest.TestCase):
+    def test_equilatero(self):
+        self.assertEqual(ladosTriangulo(3, 3, 3), "Equilátero")
+        self.assertEqual(ladosTriangulo("5", "5", "5"), "Equilátero")
+
+    def test_isoceles(self):
+        self.assertEqual(ladosTriangulo(5, 5, 3), "Isóceles")
+        self.assertEqual(ladosTriangulo(5, 3, 5), "Isóceles")
+        self.assertEqual(ladosTriangulo(3, 5, 5), "Isóceles")
+
+    def test_escaleno(self):
+        self.assertEqual(ladosTriangulo(3, 4, 5), "Escaleno")
+        self.assertEqual(ladosTriangulo("2", "3", "4"), "Escaleno")
+
+    def test_nao_possivel(self):
+        # soma de dois lados equals ou menor que o terceiro
+        self.assertEqual(ladosTriangulo(1, 2, 3), "Não é possível formar um triangulo")
+        self.assertEqual(ladosTriangulo(1, 10, 12), "Não é possível formar um triangulo")
+
+    def test_nao_valido(self):
+        # entrada não numérica
+        self.assertEqual(ladosTriangulo("a", 2, 3), "Não é válido")
+        # lados não-positivos
+        self.assertEqual(ladosTriangulo(0, 0, 0), "Não é possível formar um triangulo")
+        self.assertEqual(ladosTriangulo(-1, 2, 2), "Não é possível formar um triangulo")
+
+
+if __name__ == '__main__':
+    unittest.main()
